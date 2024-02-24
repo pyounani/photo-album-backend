84fe100bb19cf3d3c913d900f4a3512c9fe0f3c8
pyounani <yndbsk9372@gmail.com>
2024-02-24 10:19:54 +0900

    [feat] 앨범 리포지토리 생성

---
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -0,0 +1,40 @@
+package com.squarecross.photoalbum.repository;
+
+import com.squarecross.photoalbum.domain.Album;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AlbumRepository {
+
+    private final EntityManager em;
+
+    // 저장
+    public void save(Album album) {
+        em.persist(album);
+    }
+
+    // 삭제
+    public void delete(Long id) {
+        Album findAlbum = em.find(Album.class, id);
+        if(findAlbum != null) {
+            em.remove(findAlbum);
+        }
+    }
+
+    // 한 개만 조회
+    public Album findOne(Long id) {
+        return em.find(Album.class, id);
+    }
+
+    // 목록 조회
+    public List<Album> findAll() {
+        return em.createQuery("select a from Album a", Album.class)
+                .getResultList();
+    }
+
+}
