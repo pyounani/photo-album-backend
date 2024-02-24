7f2949ef9c2a1edd21b57eecb1e44da1a7bb653b
pyounani <yndbsk9372@gmail.com>
2024-02-24 10:05:53 +0900

    [feat] photo 도메인 생성

---
+++ src/main/java/com/squarecross/photoalbum/domain/Photo.java
@@ -0,0 +1,30 @@
+package com.squarecross.photoalbum.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "photo", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "photo_id")})
+public class Photo {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "photo_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "file_name", unique = false, nullable = true)
+    private String fileName;
+
+    @Column(name = "thumb_url", unique = false, nullable = true)
+    private String thumbUrl;
+
+    @Column(name = "original_url", unique = false, nullable = true)
+    private String originalUrl;
+
+    @Column(name = "file_size", unique = false, nullable = true)
+    private int fileSize;
+
+    @Column(name = "uploaded_at", unique = false, nullable = true)
+    private LocalDate uploadedAt;
+
+}
