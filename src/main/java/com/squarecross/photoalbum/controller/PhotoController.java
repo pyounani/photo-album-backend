b5b6fb2daccf1d9118634b48399f5e2460866d81
pyounani <yndbsk9372@gmail.com>
2024-03-03 17:19:22 +0900

    [feat] 하나의 사진 다운로드 API 개발 완료

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -3,13 +3,15 @@ package com.squarecross.photoalbum.controller;
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.service.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +40,25 @@ public class PhotoController {
         }
 
         return new ResponseEntity<>(photos, HttpStatus.OK);
+    }
+
+    @GetMapping("/download")
+    public void downloadPhotos(@RequestParam("photoIds") Long[] photoIds, HttpServletResponse response) {
+        try {
+            if (photoIds.length == 1) {
+                File file = photoService.getImageFile(photoIds[0]);
+                try (FileInputStream fileInputStream = new FileInputStream(file);
+                     OutputStream outputStream = response.getOutputStream()) {
+
+                    // 파일을 복사하는 작업
+                    IOUtils.copy(fileInputStream, outputStream);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error copying file", e);
+        }
     }
 
 }
