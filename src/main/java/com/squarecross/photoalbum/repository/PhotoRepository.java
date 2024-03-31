1b097613f396339c786ec87a9769d8b3ccc23331
pyounani <yndbsk9372@gmail.com>
2024-03-31 20:26:46 +0900

    [fix] 사진 서비스에도 스프링 데이터 JPA를 활용한 레포지토리로 수정

--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -4,8 +4,19 @@ import com.squarecross.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     int countByAlbum_Id(Long albumId);
+
     List<Photo> findTop4ByAlbum_IdOrderByUploadedAtDesc(Long albumId);
+
+    List<Photo> findByFileNameContainingAndAlbum_IdOrderByUploadedAtDesc(String keyword, Long albumId);
+
+    List<Photo> findByFileNameContainingAndAlbum_IdOrderByFileNameAsc(String keyword, Long albumId);
+
+    List<Photo> findByAlbum_Id(Long albumId);
+
+    Optional<Photo> findByFileNameAndAlbum_Id(String fileName, Long albumId);
+
 }
